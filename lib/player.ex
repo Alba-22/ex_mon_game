c5ed61aa3741cd82a6e514e047809adfa39869a6
Alba <alba__22@outlook.com>
2022-04-09 17:16:34 -0300

    improve Player struct

--- lib/player.ex
+++ lib/player.ex
@@ -1,11 +1,15 @@
 defmodule ExMon.Player do
+  # Variáveis de módulo
+  @required_keys [:life, :name, :move_random, :move_average, :move_heal]
+  @max_life 100
+
   # Fazer com que os campos da struct sejam obrigatórios
-  @enforce_keys [:life, :name, :move_random, :move_average, :move_heal]
-  defstruct [:life, :name, :move_random, :move_average, :move_heal]
+  @enforce_keys @required_keys
+  defstruct @required_keys
 
   def build(name, move_random, move_average, move_heal) do
     %ExMon.Player{
-      life: 100,
+      life: @max_life,
       name: name,
       move_random: move_random,
       move_average: move_average,
