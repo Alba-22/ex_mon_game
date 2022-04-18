774d9b1afe99d6fb294c307eea958de1569db944
Alba <alba__22@outlook.com>
2022-04-17 21:53:27 -0300

    print attack message

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -1,5 +1,6 @@
 defmodule ExMon.Game.Actions.Attack do
   alias ExMon.Game
+  alias ExMon.Game.Status
 
   @move_average_power 18..25
   @move_random_power 10..35
@@ -11,7 +12,7 @@ defmodule ExMon.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:life)
     |> calculate_total_life(damage)
-    |> update_opponent_life(opponent_atom)
+    |> update_opponent_life(opponent_atom, damage)
   end
 
   defp calculate_power(:move_average), do: Enum.random(@move_average_power)
@@ -20,16 +21,18 @@ defmodule ExMon.Game.Actions.Attack do
   defp calculate_total_life(life, damage) when life - damage < 0, do: 0
   defp calculate_total_life(life, damage), do: life - damage
 
-  defp update_opponent_life(life, opponent_atom) do
+  defp update_opponent_life(life, opponent_atom, damage) do
     opponent_atom
     |> Game.fetch_player()
     |> Map.put(:life, life)
-    |> update_game(opponent_atom)
+    |> update_game(opponent_atom, damage)
   end
 
-  defp update_game(player, opponent_atom) do
+  defp update_game(player, opponent_atom, damage) do
     Game.info()
     |> Map.put(opponent_atom, player)
     |> Game.update()
+
+    Status.print_move_message(opponent_atom, :attack, damage)
   end
 end
