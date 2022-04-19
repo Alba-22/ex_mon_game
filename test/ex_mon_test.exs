c66e1df5703883a5098952dc60c53834a35b71cc
Alba <alba__22@outlook.com>
2022-04-18 22:57:16 -0300

    testing ExMon module

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,7 +1,37 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert true == true
+  # Para podermos pegar as mensagens printadas na tela, já que a chamada do IO.puts() retorna :ok
+  import ExUnit.CaptureIO
+
+  alias ExMon.Player
+
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_response = %Player{
+        life: 100,
+        moves: %{move_average: :soco, move_heal: :cura, move_random: :chute},
+        name: "Alba"
+      }
+
+      assert expected_response == ExMon.create_player("Alba", :chute, :soco, :cura)
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game starts, returns a message" do
+      player = Player.build("Alba", :chute, :soco, :cura)
+
+      # Tudo que se deseja capturar deve estar dentro da função anônima de capture_io
+      messages =
+        capture_io(fn ->
+          assert ExMon.start_game(player) == :ok
+        end)
+
+      # =~  ->  Procurar na mensagem, como um Regex
+      assert messages =~ "The game has started!"
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
   end
 end
